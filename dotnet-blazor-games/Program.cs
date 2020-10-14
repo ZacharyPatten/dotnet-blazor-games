12226746a78df79071cf0bef0f0a282c9520c4d3
Zachary Patten <sevenix.zp@gmail.com>
2020-10-14 16:52:40 -0400

    updated to rc2 defaults +tictactoe fix +routing fix attempt

--- dotnet-blazor-games/Program.cs
+++ dotnet-blazor-games/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace dotnet_blazor_games
@@ -16,9 +12,7 @@ namespace dotnet_blazor_games
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
-
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 			await builder.Build().RunAsync();
 		}
 	}
